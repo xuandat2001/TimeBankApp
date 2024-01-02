176156b0a2a9b2717284757c7c4a1ff02444e06b
LamNguyen200504 <131367468+LamNguyen200504@users.noreply.github.com>
2024-01-02 21:18:01 +0700

    modify guest class

--- guest.h
+++ guest.h
@@ -1,8 +1,87 @@
-#include<iostream>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+using std::string;
 using std::cin;
 using std::cout;
 
 class Guest{
-private: 
+    friend class Admin;
+
+    protected:
+    string password;
+    int memNum = 0;
+
+    private:
+    string userName;
+    string fullName;
+    string email;
+    int phoneNumber;
+    string address;
+    int creditPoint;
+    int ratingScore;
+    string skillsInfo;
+    bool Availability;
+    int creditPoints = 0;
+
+    fstream myfile;
     
+    public:
+    void registerMember(){
+        
+        cout << "Member's information input: "; 
+        cout << "\nEnter your username: ";
+        cin >> userName;
+        cout << "\nEnter your password: ";
+        cin >> password;
+        cout << "\nEnter your fullname: ";
+        cin >> fullName;
+        cout << "\nEnter your email: ";
+        cin >> email;
+        cout << "\nEnter your phoneNumber: ";
+        cin >> phoneNumber;
+        cout << "\nEnter your address: ";
+        cin >> address;
+        cout << "\nEnter your skills: ";
+        cin >> skillsInfo;
+
+        memNum += 1;
+
+        myfile.open("pwd.dat", std::ios::app | std::ios::out); //open a file
+        myfile << password << " " << memNum << " " << userName << " " << fullName << " " << email << " " << phoneNumber << " " << address << " " << skillsInfo << "\n"; //save data to file
+        myfile.close();
+
+        creditPoints += 20;
+        cout << "Your credit points is added to a total of: " << creditPoints << "\n" << "You have registered successfully.\n";
+    }
+
+    void viewSupporters(){
+
+        int totalMembers = 0;
+
+        myfile.open("pwd.dat", std::ios::in);
+
+        if (!myfile) {
+            cout << " No data to be found\n";   
+            
+         } else {
+            myfile >> password >> memNum >> userName >> fullName >> email >> phoneNumber >> address >> skillsInfo; //take data from file and assign to variables
+            while (!myfile.eof()){ //if not at the end of file
+                cout << "Member No." << totalMembers++ << "\n"; //output to the terminal
+                cout << "Username: " << userName << "\n";
+                cout << "Full name: " << fullName << "\n";
+                cout << "Email: " << email << "\n";
+                cout << "Phone number: " << phoneNumber << "\n";
+                cout << "Address: " << address << "\n";
+                cout << "Skills: " << skillsInfo << "\n";
+                cout << endl;
+            myfile >> password >> memNum >> userName >> fullName >> email >> phoneNumber >> address >> skillsInfo; //assign again to terminate the loop
+            }
+            if (totalMembers == 0){ //check if there is no data input ever before 
+                cout << " No data to be found\n";
+            }
+        }
+        myfile.close(); 
+    }
 };
--- main.cpp
+++ main.cpp
@@ -1,13 +1,42 @@
-#include<iostream>
-#include "guest.h"
-#include "member.h"
-#include "supporter.h"
-#include "time.h"
-#include "ratingScore.h"
-#include "admin.h"
+#include <iostream>
+#include "Guest.h"
+
+using namespace std;
+using std::string;
 using std::cin;
 using std::cout;
-using std::string;
+
+
 int main(){
+    int choice;
+
+    Guest Guests;
+
+    cout << "EEET2482/COSC2082 ASSIGNMENT\n" << "TIME BANK APPLICATION\n" << "Instructor: Mr. Tran Duc Linh\n" << "Group: Group No.\n" 
+        << "sXXXXXXX, Student Name\n" << "sXXXXXXX, Student Name\n" << "sXXXXXXX, Student Name\n" << "sXXXXXXX, Student Name\n"
+        << "Use the app as 1. Guest 2. Member 3. Admin\n"
+        << "Enter your choice: ";
+
+    cin >> choice;
+
+    if (choice == 1){
+        cout << "This is your menu:\n" << "0. Exit\n" << "1. Register for a Member account\n" << "2. View supporters' details\n";
+        int choice1;
+        cout <<"Enter your choice: ";
+        cin >> choice1;
+
+        if (choice1 == 1){
+            Guests.registerMember();
+            }
+
+        else if (choice1 == 2){
+            Guests.viewSupporters();
+
+            } else {
+        cout << "Invalid choice. Please choose 1\n";
+        } 
+        } else {
+        cout << "Invalid choice. Please choose 1, 2 or 3\n";
+    }
     return 0;
 }
