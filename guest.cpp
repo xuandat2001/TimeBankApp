1c407ab585311d1f0cbd955fb4beccc6c7157362
ĐẠT <ungxuandat11092001@gmail.com>
2024-01-02 20:03:28 +0700

    FixGuestClass

--- guest.cpp
+++ guest.cpp
@@ -4,7 +4,7 @@
 #include "guest.h"
 using std::cin;
 using std::cout;
-
+using std::string;
 // Constructor of Guest class
 Guest::Guest(string username = "", string password = "",
              string fullname = "", string email = "", string homeAddress = "", string skillInfo = "",
@@ -36,42 +36,51 @@ bool Guest::registerMenber()
     else
     {
         // Get Username
-        cout << "Enter your username: ";
-        std::getline(memFile >> std::ws, username);
+            cout << "Enter your username: ";
+            std::getline(cin >> std::ws, username);
+            memFile<<"username: "<<username <<"\n";
 
-        // Get Password
-        cout << "Enter your password: ";
-        std::getline(memFile >> std::ws, password);
+            // Get Password
+            cout << "Enter your password: ";
+            std::getline(cin >> std::ws, password);
+            memFile<<"password: "<<password <<"\n";
 
-        // Get Fullname
-        cout << "Enter your Fullname: ";
-        std::getline(memFile >> std::ws, fullname);
+            // Get Fullname
+            cout << "Enter your Fullname: ";
+            std::getline(cin >> std::ws, fullname);
+            memFile<<"FullName: "<<fullname <<"\n";
 
-        // Get email
-        cout << "Enter your username: ";
-        std::getline(memFile >> std::ws, email);
+            // Get email
+            cout << "Enter your email: ";
+            std::getline(cin >> std::ws, email);
+            memFile<<"email: "<<email <<"\n";
 
-        // Get HomeAddress
-        cout << "Enter your HomeAddress: ";
-        std::getline(memFile >> std::ws, homeAddress);
+            // Get HomeAddress
+            cout << "Enter your HomeAddress: ";
+            std::getline(cin >> std::ws, homeAddress);
+            memFile<<"HomeAddress: "<<homeAddress <<"\n";
 
-        // Get Location
-        cout << "Enter your Location: ";
-        std::getline(memFile >> std::ws, location);
+            // Get Location
+            cout << "Enter your Location: ";
+            std::getline(cin >> std::ws, location);
+            memFile<<"location: "<<location <<"\n";
 
-        // Get SkillInfo
-        cout << "Enter your username: ";
-        std::getline(memFile >> std::ws, skillInfo);
+            // Get SkillInfo
+            cout << "Enter your SkillInfo: ";
+            std::getline(cin >> std::ws, skillInfo);
+            memFile<<"skillInfomation: "<<skillInfo <<"\n";
 
-        // Get PhoneNumber
-        cout << "Enter your username: ";
-        memFile >> phoneNumber;
+            // Get PhoneNumber
+            cout << "Enter your PhoneNumber: ";
+            cin >> phoneNumber;
+            memFile<<"PhoneNumber: "<<phoneNumber <<"\n";
 
-        // Get Creditpoint
-        creditPoint = 20;
-        memFile >> creditPoint;
+            // Get Creditpoint
+            creditPoint = 20;
+            memFile<<"creditPoint: "<<creditPoint <<"\n";
 
-        cout << "Register successfully";
+            memFile<<"\n";
+            cout << "Register successfully";
         
     }
     memFile.close(); // close the file
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+#include "member.h"
+#include "supporter.h"
+#include "time.h"
+#include "ratingScore.h"
+#include "admin.h"
