67a97e80f2b8c5ebe2ec9d8517e16d3ec3e2dfd3
ĐẠT <ungxuandat11092001@gmail.com>
2024-01-05 17:56:02 +0700

    UpdateMemberClass

--- member.h
+++ member.h
@@ -1,9 +1,16 @@
 #include<iostream>
+#include<vector>
+#include "guest.h"
 using std::cin;
 using std::cout;
-
-class Menber{
+using std::string;
+class Member: private Guest{
 private: 
-    
+    std::vector<int>hostRatingScore;
+    bool availability;
+    string review;
 public:
+    Member(){};
+    void getInfo();
+    
 };
